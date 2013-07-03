b2f7789da70e97db4b000ac753e6da52d7141339
Allan Yu <allan.c.yu@gmail.com>
2013-07-03 15:52:53 -0700

    commit 7.x-0.2-alpha
    minor bug fix, change way to save video file field

--- epe.dbfiles.js
+++ epe.dbfiles.js
@@ -4,8 +4,8 @@ Drupal.behaviors.epe_files = {
     var imgexts = $.parseJSON(settings.image_exts), otherexts = $.parseJSON(settings.other_exts);
     $('.form-file-field').change(function() {
       var ext = $(this).val().split('.').pop();
-      if(otherexts.length > 0 && $.inArray(ext, otherexts) > 0) { $('.form-item-files-thumbnail').show(); }
-      if(imgexts.length > 0 && $.inArray(ext, imgexts) > 0) { $('.form-item-files-thumbnail').hide(); }
+      if(otherexts.length > 0 && $.inArray(ext, otherexts) >= 0) { $('.form-item-files-thumbnail').show(); }
+      if(imgexts.length > 0 && $.inArray(ext, imgexts) >= 0) { $('.form-item-files-thumbnail').hide(); }
     });
   }
 };
