f4e2b6fc8c25eae123b66c335dd70c9324d3b413
Allan Yu <allan.c.yu@gmail.com>
2013-10-10 14:46:27 -0700

    commit 7.x-0.6-alpha
    fix permission radio button error
    add file resource menu link

--- epe.dbfiles.js
+++ epe.dbfiles.js
@@ -7,6 +7,15 @@ Drupal.behaviors.epe_files = {
       if(otherexts.length > 0 && $.inArray(ext, otherexts) >= 0) { $('.thumbnail').show(); }
       if(imgexts.length > 0 && $.inArray(ext, imgexts) >= 0) { $('.thumbnail').hide(); }
     });
+
+    $('.form-radio').click(function() {
+      console.log($(this).val());
+      if($(this).val() == 1) {
+        $('button.form-submit').removeAttr('disabled');
+      } else {
+        $('button.form-submit').attr('disabled','disabled');
+      }
+    });
   }
 };
 })(jQuery)
