a2fb0af735dd2c702dffad0dbac7790d14a86a2c
jwieclawek-rtn <jwieclawek@joe-wieclawek-mac.jpl.nasa.gov>
2013-10-11 13:31:09 -0700

    updated the proxy to only work with http:// and https:// addresses

--- php/RetrieveOWL.php
+++ php/RetrieveOWL.php
@@ -1,4 +1,14 @@
 <?php
-$owlxml = file_get_contents($_GET["owlfile"]); // return the owl doc
+
+$getURL = $_GET["owlfile"];
+$owlxml = '';
+
+if (substr($getURL, 0, 7) == 'http://') {
+	$owlxml = file_get_contents('http://' . substr($getURL, 7)); // return the owl doc
+} elseif (substr($getURL, 0, 8) == 'https://') {
+	$owlxml = file_get_contents('https://' . substr($getURL, 8)); // return the owl doc
+}
+
 echo $owlxml;
+
 ?>
