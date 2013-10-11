56219e120bc722cd22baed1a4b7b7c0ab4536e5c
Allan Yu <allan.c.yu@gmail.com>
2013-10-10 18:29:36 -0700

    code cleanup

--- app/partial/resource_browser.search-grid.html.tpl.php
+++ app/partial/resource_browser.search-grid.html.tpl.php
@@ -1,5 +1,5 @@
 <tabset>
-  <tab ng-repeat="pane in panes.table" heading="{{pane.type}} ({{pane.data.length}})" active="pane.active">
+  <tab ng-repeat="pane in panes.table" heading="{{pane.type}} ({{pane.data.length}})" active="pane.active" select="fn.activeTab(pane);">
     <ul>
     <li ng-repeat="row in pane.data | resourceFilter:query.filter | orderBy:sort:reverse">
       <div list-item
@@ -8,14 +8,3 @@
     </ul>
   </tab>
 </tabset>
-
-<!-- <tabs>
-  <pane ng-repeat="pane in panes.table" heading="{{pane.type}} ({{pane.data.length}})" active="pane.active">
-    <ul>
-    <li ng-repeat="row in pane.data | resourceFilter:query.filter | orderBy:sort:reverse">
-      <div list-item
-        checkbox="pane.show_checkbox" id="row.id" type="pane.api" url="row.url" thumbnail="row.thumbnail" title="row.title" author="row.author" updated="row.last_updated" />
-    </li>
-    </ul>
-  </pane>
-</tabs> -->
--- app/partial/resource_browser.search-list.html.tpl.php
+++ app/partial/resource_browser.search-list.html.tpl.php
@@ -16,24 +16,3 @@
   </table>
   </tab>
 </tabset>
-
-<!--
-<tabs>
-  <pane ng-repeat="pane in panes.table" heading="{{pane.type}} ({{pane.data.length}})" active="pane.active" select="fn.activeTab(pane);">
-  <table>
-    <thead>
-      <th></th>
-      <th><a ng-click="sort='title'; reverse=!reverse">Title</a></th>
-      <th><a ng-click="sort='author'; reverse=!reverse">Author</a></th>
-      <th><a ng-click="sort='last_updated'; reverse=!reverse">Mod date</a></th>
-    </thead>
-    <tbody>
-      <tr ng-repeat="row in pane.data | resourceFilter:query.filter | orderBy:sort:reverse"
-        table-row
-        checkbox="pane.show_checkbox" id="row.id" type="pane.api" url="row.url" thumbnail="row.thumbnail" title="row.title" author="row.author" updated="row.last_updated">
-      </tr>
-    </tbody>
-  </table>
-  </pane>
-</tab>
--->
