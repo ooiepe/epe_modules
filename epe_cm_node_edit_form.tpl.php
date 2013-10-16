c5900ffceae30f3c85ef90c34e1e0531ca9ade7b
jwieclawek-rtn <jwieclawek@joe-wieclawek-mac.jpl.nasa.gov>
2013-10-15 18:22:17 -0700

    updating to use latest cmb/v and incluing initial implementation of resource browser integration

--- epe_cm_node_edit_form.tpl.php
+++ epe_cm_node_edit_form.tpl.php
@@ -4,6 +4,7 @@
   $form['body']['und'][0]['value']['#title_display'] = 'invisible';
 
 
+
 ?>
 
 <style>
@@ -30,6 +31,10 @@
 
   drupal_add_js(drupal_get_path('module', 'epe_cm') . '/swf/swfobject.js');
   drupal_add_js(drupal_get_path('module', 'epe_cm') . '/swf/swfaddress/swfaddress.js');
+
+  drupal_add_js(libraries_get_path('bootbox') . '/bootbox.min.js');
+
+
 
 
   drupal_add_js('jQuery(document).ready(function () { loadFlash() });', array('type' => 'inline', 'scope' => 'footer', 'weight' => 5));
@@ -58,7 +63,8 @@ function loadFlash() {
   flashvars.USERSTATUS = '1';
   flashvars.OOI = 'true';
   flashvars.builder = 'true';
-  flashvars.RESOURCEDETAILSURL = 'http://www.resourcedetailsurl.com';
+  flashvars.DEBUG = 'false';
+  flashvars.RESOURCEDETAILSURL = '<?php echo base_path() ?>?nodelookup=';
   flashvars.PHPPROXY = '<?php echo base_path() . drupal_get_path('module', 'epe_cm') . '/php/RetrieveOWL.php' ?>';
   flashvars.OWLPATH = '<?php echo base_path() . drupal_get_path('module', 'epe_cm') . '/owl/ioos/' ?>';
   
@@ -66,7 +72,7 @@ function loadFlash() {
   var attributes = { id: 'conceptMapBuilderViewer', name: 'conceptMapBuilderViewer' };
   
   // this line is unchanged from the mwsci website
-  swfobject.embedSWF('<?php echo base_path() . drupal_get_path('module', 'epe_cm') ?>/swf/CMV_15_20131014_2050.swf', 'flashcontent', '100%', '700', '9', 'expressInstall.swf', flashvars, params, attributes);
+  swfobject.embedSWF('<?php echo base_path() . drupal_get_path('module', 'epe_cm') ?>/swf/CMV_15_20131015_1748.swf', 'flashcontent', '100%', '700', '9', 'expressInstall.swf', flashvars, params, attributes);
   
   return;
 }
@@ -104,6 +110,117 @@ function giveXMLtoJS(value) {
   document.getElementById('edit-field-cm-data-und-0-value').value = value;
   return;
 }
+
+var selectedResources = Array();
+
+function addItem(data) {
+
+  // add this item to the array
+  selectedResources.push(data);
+
+  // get the swf object
+  var swf = document.getElementById('conceptMapBuilderViewer');
+
+  // generate the xml and import to the swf
+  swf.jsToFlashImportResources(generateXMLFromItems(selectedResources));
+
+
+}
+
+function generateXMLFromItems(items) {
+  str = '';
+  strCM = '';
+  strEV = '';
+  strLLB = '';
+  strImages = '';
+  strVideos = '';
+  strDocs = '';
+
+  for (i = 0; i < items.length; i++) {
+    console.log(items[i]);
+
+    if (items[i].type == 'cm_resource') {
+      strCM += '<node id="' + items[i].nid + '" datemodified="2012-03-07 17:25:32" datemodifiedepoch="1331141132"><title><![CDATA[' + items[i].title + ']]></title><longDesc><![CDATA[' + items[i].body + ']]></longDesc><author id="4"><![CDATA[Sean Graham]]></author></node>';
+    } else if (items[i].type == 'ev_tool_instance') {
+      strEV += '<node id="' + items[i].nid + '" url="<?php echo base_path() ?>/node/' + items[i].nid + '"><title><![CDATA[' + items[i].title + ']]></title><longDesc><![CDATA[' + items[i].body + ']]></longDesc></node>';
+    } else if (items[i].type == 'llb_resource') {
+      strLLB += '<node id="' + items[i].nid + '" url="<?php echo base_path() ?>/node/' + items[i].nid + '"><title><![CDATA[' + items[i].title + ']]></title><longDesc><![CDATA[' + items[i].body + ']]></longDesc></node>';
+    } else if (items[i].type == 'image_resource') {
+      strImages += '<node id="' + items[i].nid + '" img="' + items[i].thumbnail + '" source="' + items[i].thumbnail + '" sourcew="" sourceh="" hires="" hiresw="" hiresh="" url="' + items[i].thumbnail + '"><title><![CDATA[' + items[i].title + ']]></title><longDesc><![CDATA[' + items[i].body + ']]></longDesc></node>';
+    } else if (items[i].type == 'video_resource' && items[i].type == 'audio_resource') {
+      strVideos += '<node id="' + items[i].nid + '" img="' + items[i].thumbnail + '" imgw="" imgh="" source="' + items[i].thumbnail + '" audio=""><title><![CDATA[' + items[i].title + ']]></title><longDesc><![CDATA[' + items[i].body + ']]></longDesc></node>';
+    } else if (items[i].type == 'document_resource') {
+      strDocs += '<node id="' + items[i].nid + '" url="<?php echo base_path() ?>/node/' + items[i].nid + '"><title><![CDATA[' + items[i].title + ']]></title><longDesc><![CDATA[' + items[i].body + ']]></longDesc></node>';
+    }
+
+  }
+
+  if (strCM.length > 0) 
+    str += '<conceptmaps>' + strCM + '</conceptmaps>';
+
+  if (strEV.length > 0) 
+    str += '<visualizations>' + strEV + '</visualizations>';
+
+  if (strLLB.length > 0) 
+    str += '<lessons>' + strLLB + '</lessons>';
+
+  if (strImages.length > 0) 
+    str += '<images>' + strImages + '</images>';
+
+  if (strVideos.length > 0) 
+    str += '<videos>' + strVideos + '</videos>';
+
+  if (strDocs.length > 0) 
+    str += '<docs>' + strDocs + '</docs>';
+
+
+  // add the xml tags
+  str = '<xml>' + str + '</xml>';
+
+  return str;
+}
+
+function launchResourceBrowser() {
+  
+  selectedResources.length = 0;
+
+  bootbox.dialog({
+        message: '<iframe src="' + '<?php echo base_path() ?>'  + 'dialog/resource-browser#/search" seamless width="879" height="500" class="resource-browser-iframe" />',
+        className: 'resource-browser-modal',
+        buttons: {
+          main: {
+            label: "Add Selected",
+            className: "btn btn-primary pull-left",
+            callback: function(event) {
+            var selected = [];
+            event.preventDefault();
+            var checkboxes = jQuery('.resource-browser-iframe').contents().find('input[name="nid"]');
+            checkboxes.each(function() {
+              if(jQuery(this).is(':checked')) {
+                  jQuery.ajax({
+                    url: '<?php echo base_path() ?>' + 'api/resource/' + jQuery(this).data('type') + '/' + jQuery(this).val(),
+                    dataType: 'json',
+                    async: true,
+                    success: function(data) {
+                      window.addItem(data);
+                    }
+                  });
+              }
+            });
+            }
+          },
+          cancel: {
+            label: "Cancel",
+            className: "btn"
+          }
+        }
+      });
+
+
+
+  return;
+}
+
 
 
 </script>
