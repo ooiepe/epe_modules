99962278e431f68ae5679844224dc075485f81cd
Allan Yu <allan.c.yu@gmail.com>
2013-10-16 18:15:40 -0700

    commit 7.x-0.5-alpha
    add llb homepage node & menu
    add llb homepage hook_theme, rendering template, js file & static
    content

---
+++ templates/epe_llb_homepage.tpl.php
@@ -0,0 +1,30 @@
+<div>
+  <div class="span7">
+    <div class="control-group">
+      <?php echo file_get_contents(drupal_get_path('module','epe_llb') . '/contents/homepage/homepage.html'); ?>
+    </div>
+
+    <div class="control-group">
+      <div>
+        <input type="radio" name="filter" value="" checked="checked">&nbsp;All Lessons
+        <div class="control-group <?php if(!user_is_logged_in()): echo 'well'; endif; ?>">
+          <?php if(!user_is_logged_in()): ?>
+          <div><small>Not registered? <?php echo l('Click here','user'); ?></small></div>
+          <?php endif; ?>
+        <input type="radio" name="filter" value="author" <?php if(!user_is_logged_in()): echo 'disabled'; endif; ?> >&nbsp;My Lessons Only
+        </div>
+        <input type="text" name="search_llb" size="50"> <button type="button" id="llb_submit" class="btn btn-primary">Search</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="span5">
+    <div class="control-group">
+      <img src="<?php echo drupal_get_path('module','epe_llb'); ?>/contents/homepage/homepage.jpg" />
+    </div>
+  </div>
+</div>
+
+<div>
+
+</div>
