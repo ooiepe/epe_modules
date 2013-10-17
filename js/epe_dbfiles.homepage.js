3ad2a40d6f0e67582d5939694f4c9e7a53cc7817
Allan Yu <allan.c.yu@gmail.com>
2013-10-16 18:24:03 -0700

    add resources homepage content & js

---
+++ js/epe_dbfiles.homepage.js
@@ -0,0 +1,17 @@
+(function($) {
+Drupal.behaviors.epe_dbfiles_homepage = {
+  attach: function(context, settings) {
+    $('#db_submit').on('click',function() {
+      var url = Drupal.settings.epe.base_path + 'resource-browser#/search';
+      if($('input[name=search_db]').val() != '') {
+        url += '/' + $('input[name=search_db]').val();
+      }
+      url += '?type=image';
+      if($('input:radio[name=filter]:checked').val() != '') {
+        url += '&filter=' + $('input:radio[name=filter]:checked').val();
+      }
+      window.location = url;
+    });
+  }
+};
+})(jQuery)
--- templates/epe_db_homepage.tpl.php
+++ templates/epe_db_homepage.tpl.php
@@ -1,1 +1,30 @@
-Resource homepage content placeholder
+<div>
+  <div class="span7">
+    <div class="control-group">
+      <?php echo file_get_contents(drupal_get_path('module','epe_dbfiles') . '/content/homepage/homepage.html'); ?>
+    </div>
+
+    <div class="control-group">
+      <div>
+        <input type="radio" name="filter" value="" checked="checked">&nbsp;All Resources
+        <div class="control-group <?php if(!user_is_logged_in()): echo 'well'; endif; ?>">
+          <?php if(!user_is_logged_in()): ?>
+          <div><small>Not registered? <?php echo l('Click here','user'); ?></small></div>
+          <?php endif; ?>
+        <input type="radio" name="filter" value="author" <?php if(!user_is_logged_in()): echo 'disabled'; endif; ?> >&nbsp;My Resources Only
+        </div>
+        <input type="text" name="search_db" size="50"> <button type="button" id="db_submit" class="btn btn-primary">Search</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="span5">
+    <div class="control-group">
+      <img src="<?php echo drupal_get_path('module','epe_dbfiles'); ?>/content/homepage/homepage.jpg" />
+    </div>
+  </div>
+</div>
+
+<div>
+
+</div>
