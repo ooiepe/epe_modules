0e789600a03a8e24e939b82b0186eb570da45ada
jwieclawek-rtn <jwieclawek@joe-wieclawek-mac.jpl.nasa.gov>
2013-10-25 01:48:43 -0700

    adding a wrapper div plus a class if we're coming in from the dialog url

--- app/resource_browser_main.tpl.php
+++ app/resource_browser_main.tpl.php
@@ -1,3 +1,5 @@
-<div class="main">
-  <div ng-view></div>
+<div <?php if ($_GET['q'] == 'dialog/resource-browser'): echo 'class="dialog"'; endif; ?> style="background-color: #c8d5de;padding:23px;margin-bottom:20px;">
+	<div class="main">
+  		<div ng-view></div>
+	</div>
 </div>
