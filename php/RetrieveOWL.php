4e8717352d05f1b03e70e1dbf1cf36ccbe5a910b
jwieclawek-rtn <jwieclawek@joe-wieclawek-mac.jpl.nasa.gov>
2013-11-18 12:38:27 -0800

    Updated to use curl for the proxy

--- php/RetrieveOWL.php
+++ php/RetrieveOWL.php
@@ -3,12 +3,26 @@
 $getURL = $_GET["owlfile"];
 $owlxml = '';
 
+
 if (substr($getURL, 0, 7) == 'http://') {
-	$owlxml = file_get_contents('http://' . substr($getURL, 7)); // return the owl doc
+	$owlxml = get_url_contents('http://' . substr($getURL, 7)); // return the owl doc
 } elseif (substr($getURL, 0, 8) == 'https://') {
-	$owlxml = file_get_contents('https://' . substr($getURL, 8)); // return the owl doc
+	$owlxml = get_url_contents('https://' . substr($getURL, 8)); // return the owl doc
 }
 
 echo $owlxml;
+
+
+
+function get_url_contents($url){
+        $crl = curl_init();
+        $timeout = 5;
+        curl_setopt ($crl, CURLOPT_URL,$url);
+        curl_setopt ($crl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt ($crl, CURLOPT_CONNECTTIMEOUT, $timeout);
+        $ret = curl_exec($crl);
+        curl_close($crl);
+        return $ret;
+}
 
 ?>
