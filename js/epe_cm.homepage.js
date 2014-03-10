5c08d576e85f87193cafec820a19e87af14bef38
jwieclawek-rtn <joe.wieclawek@raytheon.com>
2014-03-10 03:49:01 -0700

    EPEDEV-339 - Hitting return should initiate a search
    Added a keydown event to the js files for each homepage

--- js/epe_cm.homepage.js
+++ js/epe_cm.homepage.js
@@ -12,6 +12,19 @@ Drupal.behaviors.epe_cm_homepage = {
       }
       window.location = url;
     });
+    $('input[name=search_cm]').on('keydown',function(event) {
+      if(event.which == 13) {
+        var url = Drupal.settings.epe.base_path + 'resource-browser#/search';
+        if($('input[name=search_cm]').val() != '') {
+          url += '/' + $('input[name=search_cm]').val();
+        }
+        url += '?type=cm';
+        if($('input:radio[name=filter]:checked').val() != '') {
+          url += '&filter=' + $('input:radio[name=filter]:checked').val();
+        }
+        window.location = url;
+      }
+    });    
   }
 };
 })(jQuery)
