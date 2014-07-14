eda69cd189937815d997e304e3afce12982a426e
jwieclawek-rtn <joe.wieclawek@raytheon.com>
2014-07-14 10:23:32 -0700

    EPEDEV-370
    EPEDEV-370 - Add a listing to the detail page of displaying resources
    used within the current resource

--- epe_db/templates/linked_items.tpl.php
+++ epe_db/templates/linked_items.tpl.php
@@ -13,6 +13,15 @@ $isUsedByOtherResources = 0;
 if ($used_in_results->rowCount() > 0)
   $isUsedByOtherResources = 1;
 
+// determine if this resource includes any other resources
+$child_resources_results = db_select('resources_assoc', 'ra')
+             ->fields('ra', array('child'))
+             ->condition('parent', $node->nid)
+             ->execute();
+$hasChildResources = 0;
+if ($child_resources_results->rowCount() > 0)
+  $hasChildResources = 1;
+
 
 // determine if items have been copied from this resource this resource is used in a concept map or lesson
 $copies_of_results = db_select('field_data_field_source_nid', 'sid')
@@ -32,6 +41,9 @@ if ($copies_of_results->rowCount() > 0)
     <ul class="nav nav-tabs">
 <?php if ($isUsedByOtherResources == 1): ?>
     <li class="active"><a href="#tab_used_in" data-toggle="tab">Used in:</a></li>
+<?php endif; ?>
+<?php if ($hasChildResources == 1): ?>
+    <li><a href="#child_resources" data-toggle="tab">Resources included within this item:</a></li>
 <?php endif; ?>
 <?php if ($hasCopiesOf == 1): ?>
     <li><a href="#copies_of" data-toggle="tab">Copies:</a></li>
@@ -66,6 +78,36 @@ if ($copies_of_results->rowCount() > 0)
     </table>
     </div>
 <?php endif; ?>
+
+<?php if ($hasChildResources == 1): ?>
+    <div class="tab-pane" id="child_resources">
+    <p>Below is a list of resources included within this item:</p>
+
+    <table class="views-table cols-1 table">
+      <tr>
+        <th align="left">Resource</th>
+        <!-- <th>Type</th> -->
+      </tr>
+
+
+<?php
+
+      foreach ($child_resources_results as $result) {
+        // load the parent item
+        $child_node = node_load($result->child);
+
+      print('<tr><td><a href="' . base_path() . 'node/' . $result->child . '">');
+      print_r($child_node->title);
+      print('</a></td></tr>');
+
+      }
+
+
+?>
+    </table>
+    </div>
+<?php endif; ?>
+
 <?php if ($hasCopiesOf == 1): ?>
     <div class="tab-pane" id="copies_of">
     <p>Below is a list of resources that have been copied from this resource:</p>
