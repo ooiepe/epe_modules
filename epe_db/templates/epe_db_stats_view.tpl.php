43adea5f7742f722c28b1bcbbbe711c1e6a81855
Allan Yu <allan.c.yu@gmail.com>
2015-02-09 21:52:08 -0800

    modify view to add missing resources display create google stats page for admin and add the link to under review

---
+++ epe_db/templates/epe_db_stats_view.tpl.php
@@ -0,0 +1,77 @@
+<h1>Visualization Resources</h1>
+<div class="row">
+  <div class="col-md-4">
+    <?php echo views_embed_view('resource_statistics', 'vizmonth'); ?>
+  </div>
+  <div class="col-md-4">
+    <?php echo views_embed_view('resource_statistics', 'vizyear'); ?>
+  </div>
+  <div class="col-md-4">
+    <?php echo views_embed_view('resource_statistics', 'vizall'); ?>
+  </div>
+</div>
+
+<h1>Image Resources</h1>
+<div class="row">
+  <div class="col-md-4">
+    <?php echo views_embed_view('resource_statistics', 'imgmonth'); ?>
+  </div>
+  <div class="col-md-4">
+    <?php echo views_embed_view('resource_statistics', 'imgyear'); ?>
+  </div>
+  <div class="col-md-4">
+    <?php echo views_embed_view('resource_statistics', 'imgall'); ?>
+  </div>
+</div>
+
+<h1>Document Resources</h1>
+<div class="row">
+  <div class="col-md-4">
+    <?php echo views_embed_view('resource_statistics', 'docmonth'); ?>
+  </div>
+  <div class="col-md-4">
+    <?php echo views_embed_view('resource_statistics', 'docyear'); ?>
+  </div>
+  <div class="col-md-4">
+    <?php echo views_embed_view('resource_statistics', 'docall'); ?>
+  </div>
+</div>
+
+<h1>Multimedia Resources</h1>
+<div class="row">
+  <div class="col-md-4">
+    <?php echo views_embed_view('resource_statistics', 'multimonth'); ?>
+  </div>
+  <div class="col-md-4">
+    <?php echo views_embed_view('resource_statistics', 'multiyear'); ?>
+  </div>
+  <div class="col-md-4">
+    <?php echo views_embed_view('resource_statistics', 'multiall'); ?>
+  </div>
+</div>
+
+<h1>Data Investigation Resources</h1>
+<div class="row">
+  <div class="col-md-4">
+    <?php echo views_embed_view('resource_statistics', 'dismonth'); ?>
+  </div>
+  <div class="col-md-4">
+    <?php echo views_embed_view('resource_statistics', 'disyear'); ?>
+  </div>
+  <div class="col-md-4">
+    <?php echo views_embed_view('resource_statistics', 'disall'); ?>
+  </div>
+</div>
+
+<h1>Web Resources</h1>
+<div class="row">
+  <div class="col-md-4">
+    <?php echo views_embed_view('resource_statistics', 'webmonth'); ?>
+  </div>
+  <div class="col-md-4">
+    <?php echo views_embed_view('resource_statistics', 'webyear'); ?>
+  </div>
+  <div class="col-md-4">
+    <?php echo views_embed_view('resource_statistics', 'weball'); ?>
+  </div>
+</div>
