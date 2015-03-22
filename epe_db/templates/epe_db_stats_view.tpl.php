0a9b03f49b8b28cc31899014670581b001a4cacb
Allan Yu <allan.c.yu@gmail.com>
2015-03-22 16:57:36 -0700

    modify stats page output
    change stats page output from all stats table at onces to dropdown
    select per content type and frequency

--- epe_db/templates/epe_db_stats_view.tpl.php
+++ epe_db/templates/epe_db_stats_view.tpl.php
@@ -1,77 +1,20 @@
-<h1>Visualization Resources</h1>
-<div class="row">
-  <div class="col-md-4">
-    <?php echo views_embed_view('resource_statistics', 'vizmonth'); ?>
-  </div>
-  <div class="col-md-4">
-    <?php echo views_embed_view('resource_statistics', 'vizyear'); ?>
-  </div>
-  <div class="col-md-4">
-    <?php echo views_embed_view('resource_statistics', 'vizall'); ?>
-  </div>
-</div>
+<form method="get" name="stats_options">
+<label>Resource Type:</label>
+<select name="type">
+  <option value="viz" <?php if($params['type'] == 'viz'): echo 'selected'; endif; ?>>Visualizations</option>
+  <option value="cm" <?php if($params['type'] == 'cm'): echo 'selected'; endif; ?>>Concept Maps</option>
+  <option value="llb" <?php if($params['type'] == 'llb'): echo 'selected'; endif; ?>>Data Investigations</option>
+  <option value="file" <?php if($params['type'] == 'file'): echo 'selected'; endif; ?>>File resources</option>
+</select>
 
-<h1>Image Resources</h1>
-<div class="row">
-  <div class="col-md-4">
-    <?php echo views_embed_view('resource_statistics', 'imgmonth'); ?>
-  </div>
-  <div class="col-md-4">
-    <?php echo views_embed_view('resource_statistics', 'imgyear'); ?>
-  </div>
-  <div class="col-md-4">
-    <?php echo views_embed_view('resource_statistics', 'imgall'); ?>
-  </div>
-</div>
+<label>Frequency</label>
+<select name="frequency">
+  <option value="month" <?php if($params['frequency'] == 'month'): echo 'selected'; endif; ?>>Month</option>
+  <option value="year" <?php if($params['frequency'] == 'year'): echo 'selected'; endif; ?>>Year</option>
+  <option value="all" <?php if($params['frequency'] == 'all'): echo 'selected'; endif; ?>>All Time</option>
+</select>
 
-<h1>Document Resources</h1>
-<div class="row">
-  <div class="col-md-4">
-    <?php echo views_embed_view('resource_statistics', 'docmonth'); ?>
-  </div>
-  <div class="col-md-4">
-    <?php echo views_embed_view('resource_statistics', 'docyear'); ?>
-  </div>
-  <div class="col-md-4">
-    <?php echo views_embed_view('resource_statistics', 'docall'); ?>
-  </div>
-</div>
+<input type="submit" value="submit" />
+</form>
 
-<h1>Multimedia Resources</h1>
-<div class="row">
-  <div class="col-md-4">
-    <?php echo views_embed_view('resource_statistics', 'multimonth'); ?>
-  </div>
-  <div class="col-md-4">
-    <?php echo views_embed_view('resource_statistics', 'multiyear'); ?>
-  </div>
-  <div class="col-md-4">
-    <?php echo views_embed_view('resource_statistics', 'multiall'); ?>
-  </div>
-</div>
-
-<h1>Data Investigation Resources</h1>
-<div class="row">
-  <div class="col-md-4">
-    <?php echo views_embed_view('resource_statistics', 'dismonth'); ?>
-  </div>
-  <div class="col-md-4">
-    <?php echo views_embed_view('resource_statistics', 'disyear'); ?>
-  </div>
-  <div class="col-md-4">
-    <?php echo views_embed_view('resource_statistics', 'disall'); ?>
-  </div>
-</div>
-
-<h1>Web Resources</h1>
-<div class="row">
-  <div class="col-md-4">
-    <?php echo views_embed_view('resource_statistics', 'webmonth'); ?>
-  </div>
-  <div class="col-md-4">
-    <?php echo views_embed_view('resource_statistics', 'webyear'); ?>
-  </div>
-  <div class="col-md-4">
-    <?php echo views_embed_view('resource_statistics', 'weball'); ?>
-  </div>
-</div>
+<?php if($output) { echo $output; } ?>
