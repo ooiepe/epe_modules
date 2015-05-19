c4ad6447142f180a64e1269f347d2017f7156689
Sage Lichtenwalner <sage@marine.rutgers.edu>
2015-05-19 16:32:00 -0400

    Updated EV image export to have a white background

--- epe_ev/templates/node--ev_tool_instance.tpl.php
+++ epe_ev/templates/node--ev_tool_instance.tpl.php
@@ -74,7 +74,6 @@
 (function(){
 
   function svgToCanvas(){
-
     //load an svg snippet in the canvas
     canvg(
       document.getElementById('canvas'),
@@ -85,9 +84,20 @@
 
   function canvasToImage(){
     // save canvas image as data url (png format by default)
-      var canvas = document.getElementById("canvas"),
-        dataURL = canvas.toDataURL();
-        window.open(dataURL,"Tool Image","location=0");
+    var canvas = document.getElementById("canvas"),
+    w = canvas.width,
+    h = canvas.height;
+    
+    //create a rectangle with the desired background color
+    var destCtx = canvas.getContext('2d');
+    destCtx.globalCompositeOperation = "destination-over";
+    destCtx.fillStyle = "#FFFFFF";
+    destCtx.fillRect(0,0,w,h);
+    
+    dataURL = canvas.toDataURL('image/png');
+    dataURL.replace(/^data:image\/[^;]/, 'data:application/octet-stream');
+    
+    window.open(dataURL,"Visualization Image","location=0");
   }
 
   // set EduVis environment paths
@@ -115,10 +125,8 @@
 
   $("#tool-function-export-image")
     .on("click", function(){
-
       svgToCanvas();
       canvasToImage();
-
     });
 
 }());
