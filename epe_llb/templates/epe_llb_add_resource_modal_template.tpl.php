1456fd80c9e521340e6aabe945956a68d14ac12a
Allan Yu <allan.c.yu@gmail.com>
2015-06-09 23:25:26 -0700

    Restyle resource browser add new resource button
    bootstrap green button is confusing, remove btn-success & btn class

--- epe_llb/templates/epe_llb_add_resource_modal_template.tpl.php
+++ epe_llb/templates/epe_llb_add_resource_modal_template.tpl.php
@@ -5,7 +5,7 @@
   </div>
   <div class="modal-footer">
     <button type="button" class="btn btn-primary pull-left add-selected" data-dismiss="modal" data-api="" aria-hidden="true">Add Selected</button>
-    <a href="#adhocmodal" class="btn btn-success add-adhoc" data-api="" data-dismiss="modal" aria-hidden="true" data-toggle="modal">Add New Resource</a>
+    <a href="#adhocmodal" class="add-adhoc" data-api="" data-dismiss="modal" aria-hidden="true" data-toggle="modal">Add New Resource</a>&nbsp;&nbsp;
     <button type="button" class="btn btn-close" data-dismiss="modal" aria-hidden="true">Close</button>
   </div>
 </div>
