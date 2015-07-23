2e972d1bde9921afb2389d1276fd74447fefe543
jwieclawek-rtn <joe.wieclawek@raytheon.com>
2015-07-23 08:09:20 -0700

    Removing legacy code for cm embeds

--- epe_llb/templates/node--llb-resource.tpl.php
+++ epe_llb/templates/node--llb-resource.tpl.php
@@ -161,54 +161,6 @@
   }
 ?>
 
-<!-- cm specific js -->
-<!--
-<script type="text/javascript">
-function loadFlash() {
-  // from the microwave science website
-  var flashvars = {};
-  flashvars.USERNAME = 'sgraham';
-  flashvars.USERID = '4';
-  flashvars.USERSTATUS = '1';
-  flashvars.OOI = 'true';
-  flashvars.BUILDER = 'false';
-  flashvars.RESOURCEDETAILSURL = '<?php echo base_path() ?>api/resource/lookup?xml=';
-  flashvars.PHPPROXY = '<?php echo base_path() . drupal_get_path('module', 'epe_cm') . '/php/RetrieveOWL.php' ?>';
-  flashvars.OWLPATH = '<?php echo base_path() . drupal_get_path('module', 'epe_cm') . '/owl/ioos/' ?>';
-  var params = {};
-  var attributes = { id: 'conceptMapBuilderViewer', name: 'conceptMapBuilderViewer' };
-  // this line is unchanged from the mwsci website
-  swfobject.embedSWF('<?php echo base_path() . drupal_get_path('module', 'epe_cm') ?>/swf/CMV_15_20140114_1156.swf', 'flashcontent', '100%', '700', '9', 'expressInstall.swf', flashvars, params, attributes);
-  return;
-}
-
-function getXMLfromJS() {
-  // get the contents of the text area
-  var xml = document.getElementById('conceptMapContents').value;
-  // get a reference to the flash object
-  var swf = document.getElementById('conceptMapBuilderViewer');
-  // call the load concept map function
-  swf.jsToFlashImportMapData(xml);
-  //swf.jsToFlashImportMapData('hello');
-  return;
-}
-
-function doSave() {
-  // get a reference to the flash object
-  var swf = document.getElementById('conceptMapBuilderViewer');
-  // get the contents of the map
-  swf.getMapContents();
-  return;
-}
-
-function giveXMLtoJS(value) {
-  // put those contents into the text field
-  document.getElementById('conceptMapContents').value = value;
-  return;
-}
-</script>
--->
-
 <?php foreach($datasets as $key => $dataset): ?>
 <div class="tab-pane" id="dataset<?php echo $key; ?>">
   <ul class="breadcrumb">
