d4f913c246defadd2d64e2ecfa06d323e15a1315
Allan Yu <allan.c.yu@gmail.com>
2016-11-17 15:50:55 -0800

    merge epe-37 branch, setup video embed url

--- epe_dbvideo/templates/node--video_resource.tpl.php
+++ epe_dbvideo/templates/node--video_resource.tpl.php
@@ -13,7 +13,8 @@ $isDBFiles = 1;
 <div style="border: 1px solid #0195bd;padding:23px;margin-bottom:20px;" class="clearfix">
 
 <style type="text/css">
-.field-label {
+.field-label,
+.embed-wrapper.hidden {
   display: none;
 }
 </style>
@@ -23,7 +24,7 @@ $isDBFiles = 1;
 <legend>
   <label class="embed-toggle">Embed link</label>
   <div class="embed-wrapper hidden">
-    &lt;iframe width="560" height="315" src="<?php echo base_path() . 'node/' . arg(1) . '/embed'; ?>" frameborder="0" allowfullscreen&gt;&lt;/iframe&gt;
+    &lt;iframe width="560" height="315" src="<?php echo base_path() . 'node/' . arg(1) . '/videoembed'; ?>" frameborder="0" allowfullscreen&gt;&lt;/iframe&gt;
   </div>
 </legend>
 
